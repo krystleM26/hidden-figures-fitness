6b5b2ac0be833c2cb96d9736153de3e3f53fa653
Krystle <krystle@Krystles-MBP.lan>
2024-12-10 22:41:45 -0800

    Added Home page

--- hidden-figures-fitness/src/pages/Home.js
+++ hidden-figures-fitness/src/pages/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Header = () => {
-  return <header>Header Content</header>;
+const Home = () => {
+  return (
+    <div className='home'>
+      <section className='hero'>
+        <h1>Welcome to To Hidden Figures</h1>
+        <p>Your fitness journey starts here. Get stronger,healthier, and happier</p>
+        <button className='cta-btn'>Join Now</button>
+      </section>
+    </div>
+  )
 };
 
-export default Header;
+export default Home;

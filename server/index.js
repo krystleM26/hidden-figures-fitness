44a24a25c832e081995cf99706bee46bc7315af2
Krystle <krystle@Krystles-MBP.lan>
2024-12-11 19:00:26 -0800

    Server is running

---
+++ server/index.js
@@ -0,0 +1,10 @@
+// mongodb+srv://mitchellkrystle2:<db_password>@cluster0.67q1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+const express = require('express');
+
+const app = express();
+
+
+app.listen(3001, () => {
+    console.log('App is running')
+})

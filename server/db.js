1243c5e910ba2d007e07a0691bd8803bd03f8e47
Krystle <krystle@Krystles-MBP.lan>
2024-12-16 16:05:07 -0800

    Finised Express Server

--- server/db.js
+++ server/db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://mitchellkrystle2:test123@cluster0.67q1c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-             console.log(`MongoDB Connected: ${conn.connection.host}`)
+        const conn = await mongoose.connect(process.env.DATABASE_URI)
+            console.log(`MongoDB Connected: ${conn.connection.host}`);
+  
         } catch (error) {
-        console.log(error);
+        console.log('Database connection error:', error.message);
         process.exit(1)
     }
 }

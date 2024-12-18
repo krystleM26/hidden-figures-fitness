3216416a82212d34267bd1f30de6b034f3105e5c
Krystle <krystle@Krystles-MBP.lan>
2024-12-17 20:33:30 -0800

    Finished Server

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ connectDB()
 
 const userRoutes = require('./userRoutes.js');
 const { default: mongoose, connect } = require('mongoose');
+
+
 //middleware
 app.use(express.json());
 
@@ -16,6 +18,17 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send('API is running...');
 });
+
+app.get('/data', async (req, res) => {
+    try {
+        const data = await YourModel.find();
+        res.json(data)
+    } catch (error) {
+        res.status(500).json({message: error.message })
+    }
+})
+
+
 //Set up server
 const PORT = process.env.PORT || 3001
 

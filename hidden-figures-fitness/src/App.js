93842c3dec70dd4440680d0d6e0e70d700e1f7e9
Krystle <krystle@Krystles-MBP.lan>
2024-12-19 23:31:41 -0800

    Completed front-end

--- hidden-figures-fitness/src/App.js
+++ hidden-figures-fitness/src/App.js
@@ -1,5 +1,6 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -13,6 +14,17 @@ import Signup from './components/Signup';
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // Check if the user is logged in (check for a token in localStorage)
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+
   return (
     <Router>
       <Header />
@@ -22,13 +34,23 @@ function App() {
         <Route path="/classes" element={<Classes />} />
         <Route path="/booking" element={<Booking />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/users" element={<Profile />} />
-        <Route path="/signup" element={<Signup />} />
         
+        {/* Public Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Protected Route */}
+        <Route 
+          path="/profile" 
+          element={isLoggedIn ? <Profile /> : <Navigate to="/login" />} 
+        />
+
+        {/* Default Redirect if no match */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </Router>
+
     
    
   );
